e7ad6331e4d64a4db0f7dd9e36a6e9500cd48aa9
FranzylNatsuki <franzylbjornlmacalua@su.edu.ph>
2025-05-25 11:25:24 +0800

    TestCOntribution

--- Pokemon/obj/Debug/net9.0/Pokemon.AssemblyInfo.cs
+++ Pokemon/obj/Debug/net9.0/Pokemon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+805862832d9d97b583597139643894dd1f9c306c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646e76ee45c5b2dca3339f03c373785a6e0d0803")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
